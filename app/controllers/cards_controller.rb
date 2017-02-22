442a6ac8ae1345ccf96994cae626658453124acd
Kenn Costales <kenn@askmonolith.com>
2017-02-22 15:47:10 +0800

    created an add feature, strong params, and default review date

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,6 +10,15 @@ class CardsController < ApplicationController
   end
 
   def create
+    @card = Card.new(article_params)
 
+    @card.save
+    redirect_to @card
+  end
+
+  private
+
+  def article_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,14 @@
 class Card < ApplicationRecord
+  REVIEW_TIME_GAP = 3.freeze
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  before_validation :default_review_algorithm
+
+  protected
+
+  def default_review_algorithm
+    if review_date.nil? #getter method
+      self.review_date = DateTime.now + REVIEW_TIME_GAP #setter method
+    end
+  end
 end
